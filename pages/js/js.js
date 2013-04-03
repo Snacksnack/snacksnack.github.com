3b6d4ee9c6ac3791c502272d4dc49f4821a21ab8
Steven Koolhaas <snack93@lavabit.com>
2013-04-03 18:09:41 +0200

    Disable scrolling in overlay, TODO: Disable X-scrolling

--- pages/js/js.js
+++ pages/js/js.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	// Get an URL to display code from
 	var baseURL = 'https://api.github.com/repos/Snacksnack/';
 	var searchURLs = [
 	      			'Ruby-Colors/contents/Ruby-Colors/colors.rb',
@@ -10,7 +11,8 @@ $(document).ready(function() {
 	      			'Ruby-Pastie/contents/Ruby-Pastie/history.rb'
 	];
 	var searchURL = searchURLs[Math.floor((Math.random()*(searchURLs.length-1)))];
-
+	
+	// Get the code and display it
 	$.ajax({
 		type: 'GET',
 		url: baseURL + searchURL,
@@ -21,6 +23,10 @@ $(document).ready(function() {
 		}
 	});
 	
+    // Set background width
+	$('#content-wrapper').css('width', $(window).width()-161+'px');
+	
+	// Add listeners to image anchors to open the overlay
 	var imageLinks = $('a[href$=".png"], a[href$=".jpg"], a[href$=".gif"], a[href$=".bmp"]');
     if (imageLinks.children('img').length) {
         imageLinks.children('img').each(function() {
@@ -30,6 +36,7 @@ $(document).ready(function() {
                 e.preventDefault();
                 $('#overlay-wrapper img').attr('src', $(this).context);
                 $('#overlay-wrapper img').load(function() {
+                	$("body").css({ overflow: 'hidden' })
 	                $('#overlay-wrapper').fadeIn(5e2);
 	                $(this).attr('title', 'Click anywhere to close');
 	                $('#overlay-wrapper img').css( { "margin-left" : ($(window).width()-$('#overlay-wrapper img').width())/2, "margin-top" : ($(window).height()-$('#overlay-wrapper img').height())/2 } )
@@ -37,7 +44,9 @@ $(document).ready(function() {
         });
     }
     
+    // Add listener to overlay to close it
     $('#overlay-wrapper').click(function(e) {
+    	$("body").css({ overflow: 'visible' })
     	$(this).fadeOut(5e2);
     });
 });
