39ac16a2a03f6ffa49cc48f727ea87b4754f70ca
Steven Koolhaas <snack93@lavabit.com>
2013-04-03 21:56:37 +0200

    Removed background code

--- pages/js/js.js
+++ pages/js/js.js
@@ -1,31 +1,4 @@
 $(document).ready(function() {
-	// Get an URL to display code from
-	var baseURL = 'https://api.github.com/repos/Snacksnack/';
-	var searchURLs = [
-	      			'Ruby-Colors/contents/Ruby-Colors/colors.rb',
-	      			'Ruby-Fighters/contents/Ruby-Fighters/fighters.rb',
-	      			'dotfiles/contents/dotfiles/Console2/console.xml',
-	      			'Ircbot/contents/IrcBot/src/org/snack/irc/main/Startup.java',
-	      			'Ruby-Pastie/contents/Ruby-Pastie/paste.rb',
-	      			'Ruby-Pastie/contents/Ruby-Pastie/pastie.rb',
-	      			'Ruby-Pastie/contents/Ruby-Pastie/history.rb'
-	];
-	var searchURL = searchURLs[Math.floor((Math.random()*(searchURLs.length-1)))];
-	
-	// Get the code and display it
-	$.ajax({
-		type: 'GET',
-		url: baseURL + searchURL,
-		dataType: "jsonp",
-		crossDomain: true,
-		success: function(response){
-			$('#content-wrapper').attr('data-code', Base64.decode(response.data.content));
-		}
-	});
-	
-    // Set background width
-	$('#content-wrapper').css('width', $(window).width()-161+'px');
-	
 	// Add listeners to image anchors to open the overlay
 	var imageLinks = $('a[href$=".png"], a[href$=".jpg"], a[href$=".gif"], a[href$=".bmp"]');
     if (imageLinks.children('img').length) {
