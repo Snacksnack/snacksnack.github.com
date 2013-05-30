d7dbaf680fdde5b35a4e7928da6fc18f95d1bf18
Steven Koolhaas <snack93@lavabit.com>
2013-05-30 19:27:06 +0200

    remade the overlay wrapper

--- alt/js/js.js
+++ alt/js/js.js
@@ -1,4 +1,37 @@
 $(document).ready(function() {
+	// Hide the javascript notice
+	$('#notice').hide();
+	
+	// Add listeners to image anchors to open the overlay
+	var imageLinks = $('a[href$=".png"], a[href$=".jpg"], a[href$=".gif"], a[href$=".bmp"]');
+    if (imageLinks.children('img').length) {
+        imageLinks.children('img').each(function() {
+        	$(this).attr('title', 'Click to enlarge');
+        });
+        imageLinks.click(function(e) {
+                e.preventDefault();
+                $('#overlay-wrapper img').attr('src', $(this).context);
+	            $('#overlay-wrapper').fadeIn(5e2);
+	            $('#overlay-wrapper img').css( { 
+	            	'margin-left' : ($(window).width()-$('#overlay-wrapper img').width())/2 
+	            } );
+	            $('#overlay-inner').css( { 
+	            	'margin-left' : ($(window).width()-$('#overlay-wrapper img').width())/2, 
+	            	'margin-top' : ($(window).height()-$('#overlay-wrapper img').height())/2 - 50 
+	            } );
+	            $('#overlay-inner-close').css( { 
+	            	'margin-right' : ($(window).width()-$('#overlay-wrapper img').width())/2
+	            } );
+	            $('#overlay-inner-label').empty();
+	            $('#overlay-inner-label').append($(this).attr('title'));
+        });
+    }
+    
+    // Add listener to overlay to close it
+    $('#overlay-inner-close').click(function() {
+    	$('#overlay-wrapper').fadeOut(5e2);
+    });
+
 	// Opens/closes the menu when the menu button is clicked
 	$('#menu').click(function() {
 		$('header nav ul').fadeToggle();
@@ -9,7 +42,4 @@ $(document).ready(function() {
 	$('header nav ul a').click(function() {
 		$('header nav ul').fadeOut();
 	});
-
-	// Fades the menu on load
-	$('header nav ul').fadeOut();
 });
