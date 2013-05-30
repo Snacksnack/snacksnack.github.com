74c2a4a28850aa008989045a0ce3413cf404beba
Steven Koolhaas <snack93@lavabit.com>
2013-05-30 16:58:42 +0200

    Minor fixes, more data

--- alt/js/js.js
+++ alt/js/js.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
+	// Opens/closes the menu when the menu button is clicked
 	$('#menu').click(function() {
 		$('header nav ul').fadeToggle();
 		return false;
 	});
 
+	// CLose the menu when an option is picked
 	$('header nav ul a').click(function() {
 		$('header nav ul').fadeOut();
 	});
+
+	// Fades the menu on load
+	$('header nav ul').fadeOut();
 });
