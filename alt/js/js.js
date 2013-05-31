a2e07a36f530664be89b3f2ef6c56c53da077888
Steven Koolhaas <snack93@lavabit.com>
2013-05-31 14:24:40 +0200

    added a logo image, removed the notice, added gauto, added smooth scrolling, cleaning

--- alt/js/js.js
+++ alt/js/js.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	// Hide the javascript notice
-	$('#notice').hide();
+	// Only show menu when js is on
+	$('#menu').show();
 	
 	// Add listeners to image anchors to open the overlay
 	var imageLinks = $('a[href$=".png"], a[href$=".jpg"], a[href$=".gif"], a[href$=".bmp"]');
@@ -12,16 +12,16 @@ $(document).ready(function() {
                 e.preventDefault();
                 $('#overlay-wrapper img').attr('src', $(this).context);
 	            $('#overlay-wrapper').fadeIn(5e2);
-	            $('#overlay-wrapper img').css( { 
-	            	'margin-left' : ($(window).width()-$('#overlay-wrapper img').width())/2 
-	            } );
-	            $('#overlay-inner').css( { 
-	            	'margin-left' : ($(window).width()-$('#overlay-wrapper img').width())/2, 
-	            	'margin-top' : ($(window).height()-$('#overlay-wrapper img').height())/2 - 50 
-	            } );
-	            $('#overlay-inner-close').css( { 
-	            	'margin-right' : ($(window).width()-$('#overlay-wrapper img').width())/2
-	            } );
+	            $('#overlay-wrapper img').css({ 
+	            	'margin-left': ($(window).width()-$('#overlay-wrapper img').width())/2 
+	            });
+	            $('#overlay-inner').css({ 
+	            	'margin-left': ($(window).width()-$('#overlay-wrapper img').width())/2, 
+	            	'margin-top': ($(window).height()-$('#overlay-wrapper img').height())/2 - 50 
+	            });
+	            $('#overlay-inner-close').css({ 
+	            	'margin-right': ($(window).width()-$('#overlay-wrapper img').width())/2
+	            });
 	            $('#overlay-inner-label').empty();
 	            $('#overlay-inner-label').append($(this).attr('title'));
         });
@@ -38,8 +38,16 @@ $(document).ready(function() {
 		return false;
 	});
 
-	// CLose the menu when an option is picked
+	// Close the menu when an option is picked
 	$('header nav ul a').click(function() {
 		$('header nav ul').fadeOut();
+	});
+
+	// Smooth scrolling for anchors
+	$(".scroll").click(function(e){		
+		e.preventDefault();
+		$('html,body').animate({
+			scrollTop: $(this.hash).offset().top - 94
+		}, 1000);
 	});
 });
